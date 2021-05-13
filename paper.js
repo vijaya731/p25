2838ff3d167baf6617fe07d52220345b5bb4ae3f
vijaya731 <65881300+vijaya731@users.noreply.github.com>
2021-05-13 20:51:18 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,79 @@
+class paper
+{
+    constructor(x,y)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+			
+	}
+	{
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(World, this.body);
+
+		
+	World.add(world.this)
+		this.image=loadImage("paper.png")
+    }	
+    }    }
+  //display();
+{
+	//var paperPos=this.body.position;		
+
+	push()
+	translate(paperPos.x, paperPos.y);
+	rectMode(CENTER)
+	//strokeWeight(4);
+	fill(128,128,128)
+	rect(0,0,this.w, this.h);
+	pop()
+	
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
